11561211eac9ea4cdc94e31b89ed5af3575e4cd6
allisson012 <allissoncastilho@gmail.com>
2025-03-02 14:39:14 -0300

    adicionei um codigo para verificar se um cpf é valido

--- src/main/java/com/github/eventure/controllers/UserController.java
+++ src/main/java/com/github/eventure/controllers/UserController.java
@@ -18,4 +18,62 @@ public class UserController {
         // Return the user
         return u;
     }
+    public void validarCpf(String cpf) {
+        int [] cpfArray = ConvertCpfToArray(cpf);
+        if(IsCpf(1 , cpfArray) == true)
+        {
+            System.out.println("é um cpf valido");
+        }else
+        {
+            System.out.println("cpf nao e valido");
+        }
+       }
+       private int [] ConvertCpfToArray(String cpf)
+       {
+           int [] cpfArray = new int [11];
+           for(int i = 0; i <= 10; i++)
+           {
+               cpfArray[i] = Integer.parseInt(String.valueOf(cpf.charAt(i)));
+
+           }
+           return cpfArray;
+       }
+       private boolean IsCpf(int posicaoCodigo , int [] cpfArray) {
+           int j = 0;
+           if (posicaoCodigo == 1) {
+               j = 10;
+           } else {
+               j = 11;
+           }
+           int indexParameter = 7 + posicaoCodigo;
+
+           int resultado = 0;
+           for (int i = 0; i <= indexParameter; i++) {
+               resultado += j * cpfArray[i];
+               j--;
+           }
+           int restoDiv = resultado % 11;
+           if (restoDiv < 2) {
+               if (cpfArray[indexParameter + 1] == 0) {
+                   if (posicaoCodigo == 1) {
+                       return IsCpf(2, cpfArray);
+                   } else {
+                       return true;
+                   }
+               } else {
+                   return false;
+               }
+           } else {
+               int dif = 11 - restoDiv;
+               if (cpfArray[indexParameter + 1] == dif) {
+                   if (posicaoCodigo == 1) {
+                       return IsCpf(2,cpfArray);
+                   } else {
+                       return true;
+                   }
+               } else {
+                   return false;
+               }
+           }
+       }
 }
