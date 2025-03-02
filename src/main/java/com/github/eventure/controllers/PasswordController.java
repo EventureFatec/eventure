79c1f307a299a4d9803e26ea799ba583f500b408
kenielf <chrystian.franklin@vexpenses.com>
2025-03-02 00:54:16 -0300

    feat(encrypted-passwords): added validity verification with bitwise ops.

---
+++ src/main/java/com/github/eventure/controllers/PasswordController.java
@@ -0,0 +1,48 @@
+package com.github.eventure.controllers;
+
+import com.github.eventure.model.PasswordRules;
+
+public class PasswordController {
+    private static final short MINIMUM_LENGTH = 8;
+
+    /**
+     * Checks the password and verifies its validity.
+     *
+     * NOTE: Allowed characters: Basic Latin (0x20 -> 0x7E)
+     *
+     * @param   password    the password before it is encrypted
+     * @return              the password flags (as a short)
+     * @see                 PasswordRules
+     */
+    public short validatePasswordRules(String password) {
+        var activeRules = PasswordRules.HAS_NONE;
+
+        if (password.length() >= MINIMUM_LENGTH) activeRules |= PasswordRules.HAS_ENOUGH_CHARACTERS;
+
+        for (char c: password.toCharArray()) {
+            if (((activeRules & PasswordRules.HAS_INVALID_CHARACTERS) == 0) && (c < '!' || c > '~')) {
+                activeRules |= PasswordRules.HAS_INVALID_CHARACTERS;
+            }
+            else if (((activeRules & PasswordRules.HAS_LOWERCASE_CHARACTERS) == 0) && (c >= 'a' && c <= 'z')) {
+                activeRules |= PasswordRules.HAS_LOWERCASE_CHARACTERS;
+            }
+            else if (((activeRules & PasswordRules.HAS_UPPERCASE_CHARACTERS) == 0) && (c >= 'A' && c <= 'Z')) {
+                activeRules |= PasswordRules.HAS_UPPERCASE_CHARACTERS;
+            }
+            else if (((activeRules & PasswordRules.HAS_NUMBERS) == 0) && (c >= '0' && c <= '9')) {
+                activeRules |= PasswordRules.HAS_NUMBERS;
+            } else {
+                if ((activeRules & PasswordRules.HAS_SYMBOLS) == 0) {
+                    if ((c >= '!' && c <= '/') ||
+                        (c >= ':' && c <= '@') ||
+                        (c >= '[' && c <= '`') ||
+                        (c >= '{' && c <= '~')) {
+                        activeRules |= PasswordRules.HAS_SYMBOLS;
+                    }
+                }
+            }
+        }
+
+        return activeRules;
+    }
+}
--- src/main/java/com/github/eventure/model/PasswordRules.java
+++ src/main/java/com/github/eventure/model/PasswordRules.java
@@ -1,11 +1,18 @@
 package com.github.eventure.model;
 
+/**
+ * Uses bitwise rules to set any special flags when checking the password 
+ * validity - aside from one exception in the class, most flags are positive
+ * and must be set.
+ */
 public class PasswordRules {
-    public static final short HAS_NONE                 = 0b00000;
-    public static final short HAS_ENOUGH_CHARACTERS    = 0b00001;
-    public static final short HAS_LOWERCASE_CHARACTERS = 0b00010;
-    public static final short HAS_UPPERCASE_CHARACTERS = 0b00100;
-    public static final short HAS_NUMBERS              = 0b01000;
-    public static final short HAS_SYMBOLS              = 0b10000;
-    public static final short HAS_ALL                  = 0b11111;
+    public static final short HAS_NONE                 = 0b000000;
+    public static final short HAS_ENOUGH_CHARACTERS    = 0b000001;
+    public static final short HAS_LOWERCASE_CHARACTERS = 0b000010;
+    public static final short HAS_UPPERCASE_CHARACTERS = 0b000100;
+    public static final short HAS_NUMBERS              = 0b001000;
+    public static final short HAS_SYMBOLS              = 0b010000;
+    public static final short HAS_ALL                  = 0b011111;
+    /** This is the exception and must always be unset. */
+    public static final short HAS_INVALID_CHARACTERS   = 0b100000;
 }
