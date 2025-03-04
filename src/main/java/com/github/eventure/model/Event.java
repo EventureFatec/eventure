0522fe0ddc9a8f4be736403115c718fbecb9b7cf
allisson012 <allissoncastilho@gmail.com>
2025-03-04 11:09:06 -0300

    adicionei novos atributos para eventos

--- src/main/java/com/github/eventure/model/Event.java
+++ src/main/java/com/github/eventure/model/Event.java
@@ -1,9 +1,29 @@
 package com.github.eventure.model;
 
+import java.time.LocalTime;
+import java.util.Date;
+
 public class Event {
     private int id;
     private String name;
-
+    private String description;
+    private String title;
+    private String typeEvent;
+    private String category;
+    private Date date;
+    // não sei qual é melhor para armazenar data se é a class Date ou uma string 
+    private LocalTime startHours;
+    private LocalTime endHours;
+    // Ideia para uso seria LocalTime hora = LocalTime.of(14,35); para pegar somente hora e minutos
+    // mesma ideia da data não sei qual é melhor por causa do espaço em memoria ocupado
+    private String localNome;
+    private String adress;
+    private String street;
+    private String state;
+    private String district;
+    private String city;
+    private int cep;
+    
     public int getId() {
         return id;
     }
