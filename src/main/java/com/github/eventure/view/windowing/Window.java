645a1cfdfba8346d76a2e0f102e0fd8c7f90bd93
kenielf <chrystian.franklin@vexpenses.com>
2025-03-05 14:18:51 -0300

    feat(basic-interface): improved theme handling with look and feel.

--- src/main/java/com/github/eventure/view/windowing/Window.java
+++ src/main/java/com/github/eventure/view/windowing/Window.java
@@ -2,13 +2,32 @@ package com.github.eventure.view.windowing;
 
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+import com.formdev.flatlaf.intellijthemes.FlatLightFlatIJTheme;
+
 
 public class Window extends JFrame {
     private RootPanel panel;
 
     public Window() {
-        add(panel);
+        setupLookAndFeel();
+        setupRootPanel();
         setVisible(true);
+    }
+
+    private void setupRootPanel() {
+        panel = new RootPanel();
+        add(panel);
+    }
+
+    private void setupLookAndFeel() {
+        try {
+            UIManager.setLookAndFeel(new FlatLightFlatIJTheme());
+        } catch (UnsupportedLookAndFeelException e) {
+            System.out.println(e.getStackTrace());
+        }
     }
 
     public static void run() {
