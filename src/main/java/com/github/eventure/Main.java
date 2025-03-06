8401160f85a68833f89dc9318f0ba7bf731a4b8c
allisson012 <allissoncastilho@gmail.com>
2025-03-06 18:48:39 -0300

    Criei a classe EmailController para verificar se um email é valido. criei o metodo para deletar usuario e criei o construtor da classe userController

--- src/main/java/com/github/eventure/Main.java
+++ src/main/java/com/github/eventure/Main.java
@@ -1,8 +1,12 @@
 package com.github.eventure;
 
 import com.github.eventure.storage.Storage;
+
+import java.util.List;
+
 import com.github.eventure.controllers.UserController;
 import com.github.eventure.model.Event;
+import com.github.eventure.model.User;
 
 public class Main {
     public static void main(String[] args) {
@@ -29,9 +33,19 @@ public class Main {
         // Create a user
         var userController = new UserController();
         var user = userController.createUser("Chrystian", "Mendes Franklin", "exemplo123");
+        
+
         System.out.println(user.getName());
         System.out.println(user.getPasswordHash().toString());
         String cpf = "29640340871";
         userController.validarCpf(cpf);
+//        userController.deleteUser(user);
+        
+        List<String> emails = userController.findUser("Mendes Franklin");
+        System.out.println("\nEmail do usuario com nome Chrystian Mendes Franklin:");
+        emails.forEach(System.out::println);
+  
+        
+        
     }
 }
