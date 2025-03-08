e3eb7b2c571638aaf7c30c01f401adc7d7b8e656
kenielf <chrystian.franklin@vexpenses.com>
2025-03-08 09:32:41 -0300

    fix(basic-interface)!: moved the previous root panel to a dedicated layer, to accomodate multi-layering.

--- src/main/java/com/github/eventure/view/windowing/layers/ContentLayer.java
+++ src/main/java/com/github/eventure/view/windowing/layers/ContentLayer.java
@@ -1,27 +1,41 @@
-package com.github.eventure.view.windowing;
+package com.github.eventure.view.windowing.layers;
 
 import java.awt.CardLayout;
+import java.awt.Color;
 import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Queue;
 
+import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
 
 import com.github.eventure.reflection.Reflection;
+import com.github.eventure.view.windowing.Page;
 
-public class RootPanel extends JPanel {
-    // TODO: Add notification support and layered panes (content, modal and pop-ups)
+import net.miginfocom.swing.MigLayout;
+
+public class ContentLayer extends JPanel {
     private static final String PAGES_PACKAGE = "com.github.eventure.view.pages";
     private Queue<String> pagesHistory;
     private HashMap<String, Page> pagesAdded;
     private String currentPageId;
     private CardLayout layout;
 
-    public RootPanel() {
-        setupLayout();
-        addPagesViaReflection();
+    public ContentLayer() {
+        setBackground(Color.PINK);
+        setupContent();
+        // setupLayout();
+        // addPagesViaReflection();
     }
 
+    private void setupContent() {
+        JLayeredPane rootPanel = new JLayeredPane();
+        MigLayout customLayout = new MigLayout();
+        JPanel contentPanel = new JPanel(customLayout);
+        contentPanel.setBackground(Color.BLUE);
+        rootPanel.add(contentPanel, JLayeredPane.DEFAULT_LAYER);
+        add(rootPanel);
+    }
     private void setupLayout() {
         layout = new CardLayout();
         setLayout(layout);
