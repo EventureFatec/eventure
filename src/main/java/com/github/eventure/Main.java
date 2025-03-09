f4da0daab178f917770e65c263c798d9ba1c38b9
allisson012 <allissoncastilho@gmail.com>
2025-03-09 14:01:31 -0300

    adicionei o metodo findUserById

--- src/main/java/com/github/eventure/Main.java
+++ src/main/java/com/github/eventure/Main.java
@@ -9,47 +9,37 @@ import com.github.eventure.model.Event;
 import com.github.eventure.model.User;
 
 public class Main {
-    public static void main(String[] args) {
-        // Armazenamento
-        var eventStorage = new Storage<Event>();
+	public static void main(String[] args) {
+		// Armazenamento
+		var eventStorage = new Storage<Event>();
 
-        // Criar todos os eventos
-        String[] names = { "Acelera Fatec", "Kapota", "Palestra Fatec", "Carnaval" };
-        int counter = 0;
-        for (String name: names) {
-            var event = new Event();
-            event.setId(++counter);
-            event.setName(name);
-            eventStorage.add(event);
-        }
+		// Criar todos os eventos
+		String[] names = { "Acelera Fatec", "Kapota", "Palestra Fatec", "Carnaval" };
+		int counter = 0;
+		for (String name : names) {
+			var event = new Event();
+			event.setId(++counter);
+			event.setName(name);
+			eventStorage.add(event);
+		}
 
-        // Encontrar eventos da Fatec
-        for (Event event: eventStorage.find(
-            event -> event.getName().contains("Fatec")
-        ).toList()) {
-            System.out.println(event.toString());
-        }
+		// Encontrar eventos da Fatec
+		for (Event event : eventStorage.find(event -> event.getName().contains("Fatec")).toList()) {
+			System.out.println(event.toString());
+		}
 
-        // Create a user
-        var userController = new UserController();
-        var user = userController.createUser("Chrystian", "Mendes Franklin", "exemplo123","Chrystian@gmail.com");
-        var user02 = userController.createUser("allisson", "thomas", "1234@E","allisson@gmail.com");
+		// Create a user
+		var userController = new UserController();
+		var user = userController.createUser("Chrystian", "Mendes Franklin", "exemplo123", "Chrystian@gmail.com",
+				"52998224725");
 
-        System.out.println(user.getName());
-        System.out.println(user.getPasswordHash().toString());
-        String cpf = "29640340871";
-        userController.validarCpf(cpf);
-//        userController.deleteUser(user);
-        System.out.println("id do chrystian "+user.getUserId());
-        System.out.println("id do allisson "+user02.getUserId());
-        List<String> emails = userController.findUser("Mendes Franklin");
-        userController.deleteUser(user02);
-        var user03 = userController.createUser("pedro", "lucas", "1234@E","pedro@gmail.com");
-        System.out.println("id do pedro "+user03.getUserId());
-        System.out.println("\nEmail do usuario com nome Chrystian Mendes Franklin:");
-        emails.forEach(System.out::println);
-  
-        
-        
-    }
+		System.out.println(user.getName());
+		System.out.println(user.getPasswordHash().toString());
+		System.out.println(user.getCpf());
+		System.out.println("id do chrystian " + user.getUserId());
+
+		User user02 = userController.findUserById(0);
+
+		System.out.println(user02.getName());
+	}
 }
--- src/main/java/com/github/eventure/controllers/UserController.java
+++ src/main/java/com/github/eventure/controllers/UserController.java
@@ -10,110 +10,120 @@ import com.github.eventure.storage.Storage;
 public class UserController {
 	private Storage<User> userStorage;
 	private static int lastGeneratedId = 0;
-	public UserController()
-	{
-		if(userStorage == null)
-		{
+
+	public UserController() {
+		if (userStorage == null) {
 			userStorage = new Storage<User>();
 		}
 	}
-    public User createUser(String firstName, String lastName, String password , String email) {
-        // Instantiate the user
-        var u = new User();
-        u.setName(firstName + " " + lastName);
-        EmailController emailTest = new EmailController();
-        boolean verdade_ou_nao = emailTest.ValidarEmail(email);
-        System.out.println("email é :"+verdade_ou_nao);
-        if(verdade_ou_nao)
-        {
-        u.setEmail(emailTest.getEmail());
-        }
-        // Create the password hash
-        var salt = Encryption.generateSalt();
-        var hash = Encryption.generateHash(password, salt);
-        u.setPasswordSalt(salt);
-        u.setPasswordHash(hash);
-        
-        if(u.getName()  != null && u.getPasswordHash() != null && u.getName() != null)
-        {
-        	int id = UserController.generateId();
-        	u.setUserId(id);
-        }
-        userStorage.add(u);
-        
-        // Return the user
-        return u;
-    }
-    
-    public void deleteUser(User u)
-    {
-    	userStorage.remove(u);
-    }
-    
-    public List<String> findUser(String lastName)
-    {
-    	List<String> emails = userStorage.find(user -> user.getName().equals("Chrystian Mendes Franklin")).map(User::getEmail).collect(Collectors.toList());
-    	return emails;
-    }
 
-    public void validarCpf(String cpf) {
-        int[] cpfArray = ConvertCpfToArray(cpf);
-        if (IsCpf(1, cpfArray) == true) {
-            System.out.println("é um cpf valido");
-        } else {
-            System.out.println("cpf nao e valido");
-        }
-    }
+	public User createUser(String firstName, String lastName, String password, String email, String cpf) {
+		// Instantiate the user
+		var u = new User();
+		u.setName(firstName + " " + lastName);
+		EmailController emailTest = new EmailController();
+		boolean verdade_ou_nao = emailTest.ValidarEmail(email);
 
-    private int[] ConvertCpfToArray(String cpf) {
-        int[] cpfArray = new int[11];
-        for (int i = 0; i <= 10; i++) {
-            cpfArray[i] = Integer.parseInt(String.valueOf(cpf.charAt(i)));
+		if (verdade_ou_nao) {
+			u.setEmail(emailTest.getEmail());
+		}
+		boolean cpfValid = validarCpf(cpf);
 
-        }
-        return cpfArray;
-    }
+		if (cpfValid) {
+			u.setCpf(cpf);
+		}
+		// Create the password hash
+		var salt = Encryption.generateSalt();
+		var hash = Encryption.generateHash(password, salt);
+		u.setPasswordSalt(salt);
+		u.setPasswordHash(hash);
 
-    private boolean IsCpf(int posicaoCodigo, int[] cpfArray) {
-        int j = 0;
-        if (posicaoCodigo == 1) {
-            j = 10;
-        } else {
-            j = 11;
-        }
-        int indexParameter = 7 + posicaoCodigo;
+		if (u.getName() != null && u.getPasswordHash() != null && u.getName() != null && cpfValid) {
+			int id = UserController.generateId();
+			u.setUserId(id);
+			userStorage.add(u);
+		}
 
-        int resultado = 0;
-        for (int i = 0; i <= indexParameter; i++) {
-            resultado += j * cpfArray[i];
-            j--;
-        }
-        int restoDiv = resultado % 11;
-        if (restoDiv < 2) {
-            if (cpfArray[indexParameter + 1] == 0) {
-                if (posicaoCodigo == 1) {
-                    return IsCpf(2, cpfArray);
-                } else {
-                    return true;
-                }
-            } else {
-                return false;
-            }
-        } else {
-            int dif = 11 - restoDiv;
-            if (cpfArray[indexParameter + 1] == dif) {
-                if (posicaoCodigo == 1) {
-                    return IsCpf(2, cpfArray);
-                } else {
-                    return true;
-                }
-            } else {
-                return false;
-            }
-        }
-    }
-    public static int generateId() 
-        { 
-    	return lastGeneratedId++; 
-    	}
+		// Return the user
+		return u;
+	}
+
+	public void deleteUser(User u) {
+		userStorage.remove(u);
+	}
+
+	public User findUserById(int id) {
+		return userStorage.find(user -> user.getUserId() == id).findFirst().orElse(null);
+	}
+
+	public boolean validarCpf(String cpf) {
+		
+		boolean valid = false;
+	    if (cpf.length() != 11 || cpf.matches("(\\d)\\1{10}")) {
+	        System.out.println("CPF inválido");
+	        return false;
+	    }
+	    
+	    int[] cpfArray = ConvertCpfToArray(cpf);
+	    
+		if (IsCpf(1, cpfArray) == true) {
+			System.out.println("é um cpf valido");
+			valid = true;
+		} else {
+			System.out.println("Cpf nao e valido");
+		}
+		return valid;
+	}
+
+	private int[] ConvertCpfToArray(String cpf) {
+		int[] cpfArray = new int[11];
+		for (int i = 0; i <= 10; i++) {
+			cpfArray[i] = Integer.parseInt(String.valueOf(cpf.charAt(i)));
+		}
+		return cpfArray;
+	}
+
+	private boolean IsCpf(int posicaoCodigo, int[] cpfArray) {
+		int j = 0;
+
+		if (posicaoCodigo == 1) {
+			j = 10;
+		} else {
+			j = 11;
+		}
+		int indexParameter = 7 + posicaoCodigo;
+
+		int resultado = 0;
+		for (int i = 0; i <= indexParameter; i++) {
+			resultado += j * cpfArray[i];
+			j--;
+		}
+		int restoDiv = resultado % 11;
+		if (restoDiv < 2) {
+			if (cpfArray[indexParameter + 1] == 0) {
+				if (posicaoCodigo == 1) {
+					return IsCpf(2, cpfArray);
+				} else {
+					return true;
+				}
+			} else {
+				return false;
+			}
+		} else {
+			int dif = 11 - restoDiv;
+			if (cpfArray[indexParameter + 1] == dif) {
+				if (posicaoCodigo == 1) {
+					return IsCpf(2, cpfArray);
+				} else {
+					return true;
+				}
+			} else {
+				return false;
+			}
+		}
+	}
+
+	public static int generateId() {
+		return lastGeneratedId++;
+	}
 }
