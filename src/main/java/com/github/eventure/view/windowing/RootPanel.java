37902f6a3a1b52458cf1db34d1337e44b6cfde98
kenielf <chrystian.franklin@vexpenses.com>
2025-03-18 04:27:47 -0300

    fix(dvanced-interface): fix unmerged root panel.

--- src/main/java/com/github/eventure/view/windowing/RootPanel.java
+++ src/main/java/com/github/eventure/view/windowing/RootPanel.java
@@ -1,20 +1,6 @@
 package com.github.eventure.view.windowing;
 
 import javax.swing.JLayeredPane;
-<<<<<<< Updated upstream
-import javax.swing.OverlayLayout;
-
-import com.github.eventure.view.windowing.layers.ContentLayer;
-
-public class RootPanel extends JLayeredPane {
-    private OverlayLayout layout;
-
-    public RootPanel() {
-        super();
-
-        setupLayout();
-        setupAlignment();
-=======
 import javax.swing.JPanel;
 import javax.swing.OverlayLayout;
 
@@ -25,24 +11,15 @@ public class RootPanel extends JLayeredPane {
 
     public RootPanel() {
         setupLayout();
->>>>>>> Stashed changes
         setupLayers();
     }
 
     private void setupLayout() {
-<<<<<<< Updated upstream
-        layout = new OverlayLayout(this);
-        setLayout(layout);
-    }
-
-    private void setupAlignment() {
-=======
         // Layout
         panelLayout = new OverlayLayout(this);
         setLayout(panelLayout);
 
         // Alignment
->>>>>>> Stashed changes
         setAlignmentX(CENTER_ALIGNMENT);
         setAlignmentY(CENTER_ALIGNMENT);
     }
