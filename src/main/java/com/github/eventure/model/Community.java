13405d8e32d9eb680b2e98da56492fcf883acfc8
allisson012 <allissoncastilho@gmail.com>
2025-04-04 13:51:23 -0300

    adicionei a classe Community e message com seus atributos e adicionei a classe CommunityController e criei o metodo createCommunity findCommunity e addUser

--- src/main/java/com/github/eventure/model/Community.java
+++ src/main/java/com/github/eventure/model/Community.java
@@ -1,8 +1,79 @@
 package com.github.eventure.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Community {
- private List <User> users;
- private List <Message> mensagens;
+	private String name;
+	private String description;
+	private int idMaker;
+	// id do criador para diferenciar eles dos demais usuarios sendo ele que
+	// controla quem ele coloca na comunidade
+	private int idCommunity;
+	// a ideia é ter o id dos usuarios participantes da comunidade para não precisar
+	// armazenar o Usuario diretamente
+	// a ideia é que os usuarios tenha uma area no programa aonde seja exibido as
+	// comunidades que eles participam
+	private List<Integer> IdUsers = new ArrayList<>();
+	private List<Message> mensagens;
+
+	// mensagem seria um conjunto de label para dar uma formatação a mensagem.
+	// mensagem ia ter = nome do usuario , horario de envio e a propria mensagem
+	public Community(String name, int idMaker) {
+		this.name = name;
+		this.idMaker = idMaker;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void addUserList(int idUser) {
+		IdUsers.add(idUser);
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getIdMaker() {
+		return idMaker;
+	}
+
+	public void setIdMaker(int idMaker) {
+		this.idMaker = idMaker;
+	}
+
+	public int getIdCommunity() {
+		return idCommunity;
+	}
+
+	public void setIdCommunity(int idCommunity) {
+		this.idCommunity = idCommunity;
+	}
+
+	public List<Integer> getIdUsers() {
+		return IdUsers;
+	}
+
+	public void setIdUsers(List<Integer> idUsers) {
+		IdUsers = idUsers;
+	}
+
+	public List<Message> getMensagens() {
+		return mensagens;
+	}
+
+	public void setMensagens(List<Message> mensagens) {
+		this.mensagens = mensagens;
+	}
+
 }
--- src/main/java/com/github/eventure/model/Message.java
+++ src/main/java/com/github/eventure/model/Message.java
@@ -1,8 +1,11 @@
 package com.github.eventure.model;
 
+import java.time.LocalTime;
+
 public class Message {
-  private String message;
-  // hora 
-  private String name;
-  private int idUser;
+	private String message;
+	private LocalTime localTime;
+	// guardar o horario que a mensagem foi enviada
+	private String name;
+	private int idUser;
 }
