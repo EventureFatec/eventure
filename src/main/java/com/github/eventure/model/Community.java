c240232961ed7fb52f0323d2b0bc9b21877e222b
allisson012 <allissoncastilho@gmail.com>
2025-04-04 10:08:35 -0300

    adicionando a classe community e adicionando atributos

---
+++ src/main/java/com/github/eventure/model/Community.java
@@ -0,0 +1,8 @@
+package com.github.eventure.model;
+
+import java.util.List;
+
+public class Community {
+ private List <User> users;
+ private List <Message> mensagens;
+}
---
+++ src/main/java/com/github/eventure/model/Message.java
@@ -0,0 +1,8 @@
+package com.github.eventure.model;
+
+public class Message {
+  private String message;
+  // hora 
+  private String name;
+  private int idUser;
+}
