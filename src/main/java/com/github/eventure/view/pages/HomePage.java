3dc969413e32c4561710368d9dc835ee8adea9be
allisson012 <allissoncastilho@gmail.com>
2025-04-17 16:22:26 -0300

    criando o metodo para criar evento com viaCep e adicionando classe imageController

--- src/main/java/com/github/eventure/view/pages/HomePage.java
+++ src/main/java/com/github/eventure/view/pages/HomePage.java
@@ -8,20 +8,24 @@ import net.miginfocom.swing.MigLayout;
 
 import com.github.eventure.view.windowing.basic.Page;
 import com.github.eventure.view.windowing.basic.PageLayouts;
+import com.github.eventure.controllers.ImageController;
 import com.github.eventure.model.Session;
 import com.github.eventure.view.windowing.basic.Notification;
 
 public class HomePage extends Page {
+	private String imagePath = "";
+	private JPanel p;
     public HomePage() {
         super(PageLayouts.GALLERY_LAYOUT);
          setDefault(true);
+         
 
         var gallery = new JLabel("GALERIA");
         gallery.setFont(gallery.getFont().deriveFont(48f));
         add(gallery, "span");
         add(new JLabel("Example 1"), "wrap");
         System.out.println("test");
-        var p = new JPanel(new MigLayout("fillx", PageLayouts.REGULAR_LAYOUT));
+        p = new JPanel(new MigLayout("fillx", PageLayouts.REGULAR_LAYOUT));
         var b = new JButton("Send Notification");
         b.addActionListener(event -> {
             sendNotification(new Notification("example notification"));
@@ -34,11 +38,11 @@ public class HomePage extends Page {
         b2.addActionListener(event -> switchPage(getPageId(SecondaryPage.class)));
         p.add(b2, "wrap");
         p.add(b, "growx, span");
-
+        
         add(p, "");
         add(new JLabel("Example 3"), "span");
     }
-
+ 
     private void tryLogout() {
         Session.logout();
         switchPage(getPageId(LoginPage.class));
